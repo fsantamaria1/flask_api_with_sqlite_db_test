1535978b6b92585a48d50f17df1ffaa5be5157d1
Fredy Santamaria <fsabtamaria@berryit.net>
2022-10-13 11:15:03 -0400

    Configured the get_one_todo route

--- complex_test_api (with db containing user data).py
+++ complex_test_api (with db containing user data).py
@@ -179,7 +179,16 @@ def get_all_todos(current_user):
 @app.route('/todo/<todo_id>', methods=['GET'])
 @token_required
 def get_one_todo(current_user, todo_id):
-    return ''
+    #Limit by user
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message': 'No todo found!'})
+    todo_data = {}
+    todo_data['id'] = todo.id
+    todo_data['text'] = todo.text
+    todo_data['complete'] = todo.complete
+    return jsonify(todo_data)
 
 @app.route('/todo', methods=['POST'])
 @token_required
