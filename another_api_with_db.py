4839d7275e18e2bd6d4bca8e3ec976f9aacf00b0
Fredy Santamaria <fsabtamaria@berryit.net>
2022-10-19 14:04:12 -0400

    Added Error Handling to the CreateUsers Route

--- another_api_with_db.py
+++ another_api_with_db.py
@@ -32,6 +32,40 @@ class Timesheet(db.Model):
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
+@app.route('/user', methods=['POST'])
+def create_users():
+
+    try:
+        # Get data from request
+        data = request.get_json()
+        # if not data["username"]:
+        if not data.get("username"):
+            return jsonify({'message': 'No username found!'}), 400
+        elif not data.get("password"):
+            return jsonify({'message': 'No password found!'}), 400
+        # elif len(data["username"]) <5:
+        elif len(data.get("username")) <5:
+            return jsonify({'message': 'Username too short!'}), 400
+        elif len(data["password"]) <5:
+            return jsonify({'message': 'Password too short!'}), 400
+
+    except:
+        return jsonify({'message': 'No data found!'}), 400
+
+    # Query dabatase to see if the username already exists
+    existing_user = User.query.filter_by(username=data["username"]).first()
+
+    if not existing_user:
+        hashed_password= generate_password_hash(data['password'], method='sha256')
+
+        #Create new user
+        new_user = User(public_id=str(uuid.uuid4()), username=data['username'], password=hashed_password, manager=False, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({'message': 'New user created!'})
+    else:
+        return jsonify({"message": "User already exists!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
